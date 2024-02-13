394f73396cc53b1880ac2ee60d30b3afaee56154
betheproud <betheproud@gmail.com>
2024-02-13 15:33:56 +0900

    headless 추가

--- scheduler/faucet/berachain.py
+++ scheduler/faucet/berachain.py
@@ -1,4 +1,5 @@
 import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -9,9 +10,19 @@ from system.client.discord.faucet_discrod_client import FaucetDiscordClient
 def faucet_berachain_job() -> None:
     print("faucet-berachain")
 
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    )
+    options.add_argument("lang=ko_KR")
+    options.add_argument("no-sandbox")
+
+    driver = webdriver.Chrome(options=options)
     driver.get(url="https://artio.faucet.berachain.com/")
-    time.sleep(2)
+    driver.implicitly_wait(3)
 
     local_storage = LocalStorage(driver)
     local_storage.set("FIRST_TIME_USER", "false")
@@ -26,8 +37,8 @@ def faucet_berachain_job() -> None:
     claim_button.click()
     time.sleep(5)
 
-    # drip_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/main/div/div[1]/div/button')
-    # drip_button.click()
+    drip_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/main/div/div[1]/div/button')
+    drip_button.click()
 
     FaucetDiscordClient().faucet_done('berachain')
 
--- scheduler/jobs.py
+++ scheduler/jobs.py
@@ -9,7 +9,7 @@ from scheduler.faucet.berachain import faucet_berachain_job
 def start():
     scheduler = BackgroundScheduler(timezone='Asia/Seoul')
     # scheduler.add_jobstore(DjangoJobStore(), 'default')
-    scheduler.add_job(faucet_berachain_job, 'interval', minutes=1, name='faucet-berachain', max_instances=1)
+    scheduler.add_job(faucet_berachain_job, 'interval', hours=8, minutes=5, name='faucet-berachain', max_instances=1)
     # scheduler.add_job(scraper, 'interval', seconds=30, name='scraper-me')
     scheduler.start()
     atexit.register(lambda: scheduler.shutdown())
