55729ecd6f3850476759867f83ad7ec14c4a45b4
betheproud <betheproud@gmail.com>
2024-02-14 15:22:13 +0900

    Update jobs.py

--- scheduler/jobs.py
+++ scheduler/jobs.py
@@ -9,8 +9,8 @@ from scheduler.faucet.berachain_sapo import faucet_berachain_job_sapo
 def start():
     scheduler = BackgroundScheduler(timezone='Asia/Seoul')
 
-    scheduler.add_job(faucet_berachain_job, 'interval', hours=4, minutes=5, name='faucet-berachain', max_instances=1)
-    scheduler.add_job(faucet_berachain_job_sapo, 'interval', hours=4, minutes=5, name='faucet-berachain-sapo', max_instances=1)
+    scheduler.add_job(faucet_berachain_job, 'interval', hours=4, name='faucet-berachain', max_instances=1)
+    scheduler.add_job(faucet_berachain_job_sapo, 'interval', hours=4, name='faucet-berachain-sapo', max_instances=1)
     # scheduler.add_job(faucet_berachain_job, 'interval', minutes=1, name='faucet-berachain', max_instances=1)
     # scheduler.add_job(faucet_berachain_job_sapo, 'interval', minutes=1, name='faucet-berachain', max_instances=1)
     # scheduler.add_job(scraper, 'interval', seconds=30, name='scraper-me')
